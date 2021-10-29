708d6e89ca55d1d8b6388afc9d50c5b9cf357f18
Justin-Chen <justin.uob@gmail.com>
2021-10-29 12:26:56 +0800

    40

--- src/leetcode/editor/en/CombinationSum_39_2.java
+++ src/leetcode/editor/en/CombinationSum_39_2.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-public class CombinationSum_39 {
+public class CombinationSum_39_2 {
 
     public static void main(String[] args) {
-        Solution solution = new CombinationSum_39().new Solution();
+        Solution solution = new CombinationSum_39_2().new Solution();
         int[] candidates = {2,3,6,7};
         int target = 7;
         solution.combinationSum(candidates,target);
@@ -18,28 +18,24 @@ public class CombinationSum_39 {
 
         List<List<Integer>> res = new LinkedList<>();
         int[] nums = null;
-        int target = 0;
 
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
             Arrays.sort(candidates);
             this.nums = candidates;
-            this.target = target;
-            dfs(new LinkedList<>(), 0);
+            dfs(new LinkedList<>(), 0, target);
             return res;
         }
 
-        private void dfs(List<Integer> currentList, int index) {
+        private void dfs(List<Integer> currentList, int index, int target) {
             if(target==0){ // 減至零時
                 res.add(new LinkedList<>(currentList));
             }else {
                 for(int i = index; i<nums.length; i++){ // 0, 1, 2, 3
                     if(nums[i]>target) // 減完後小於零就break loop(超過base case，之後就不必做了)
                         break;
-                    target = target - nums[i];
                     currentList.add(nums[i]);
-                    dfs(currentList,i); // 0, 1, 2, 3
+                    dfs(currentList,i,target - nums[i]); // target-當前的數，進入下一層
                     currentList.remove(currentList.size()-1);
-                    target += nums[i];
                 }
             }
         }
